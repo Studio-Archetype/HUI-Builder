c3c335769982f15d49ff23b1d085a8f95894f6bb
Wesley Breukers <wesley@vertcodedevelopment.com>
2023-06-26 01:19:11 +0200

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,37 @@
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-    output: 'export',
-    basePath: '/nextjs-github-pages',
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+     */
+    output: "export",
+
+    /**
+     * Enable strict mode for React. This will help identify legacy code that
+     *
+     * @see https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
+     */
     reactStrictMode: true,
+
+    /**
+     * Set base path. This is usually the slug of your repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/nextjs-github-pages",
+
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+     */
     images: {
         unoptimized: true,
     },
 };
+
+module.exports = nextConfig;
