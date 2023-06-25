ce665fd7fb7cbf58babfbcf2565eef7a6f16b127
Wesley Breukers <wesley@vertcodedevelopment.com>
2023-06-26 01:25:23 +0200

    fixed basepath

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/nextjs-github-pages",
+    basePath: "/",
 
     /**
      * Disable server-based image optimization. Next.js does not support
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <a className={styles.brand} href="https://studioarchetype.net/" target={"_blank"}>
                 <div className={styles.brandImage}>
                     <Image
-                        src={"/nextjs-github-pages/logo.svg"}
+                        src={"/logo.svg"}
                         alt={"StudioArchetype Logo"}
                         fill
                     />
